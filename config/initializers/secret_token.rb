74391daeeea95b0203f11f7e9e4ab70ace30c994
kashifali17 <kashif.tufail17@gmail.com>
2012-06-16 16:02:05 +0500

    add file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WickedPdfGenerate::Application.config.secret_token = '4f1f56acc6880bdf1d96fafe2cd67d682153d891fca95857e5d9a79ae17b06bc979a79a0552710df4ba7b980fc0c618ab4a96ba312057fa45fc61ac303bed745'
---
+++ config/routes.rb
@@ -0,0 +1,74 @@
+WickedPdfGenerate::Application.routes.draw do
+
+
+  match "/posts/download_pdf(.:format)" => "posts#all_posts", :method => :get, :as=>:all_posts
+
+  resources :posts do
+    collection do
+      get :all_posts
+    end
+  end
+
+  devise_for :users
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  devise_for :users do
+    get "sign_out", :to => "devise/sessions#destroy"
+  end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+   root :to => 'posts#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+end
