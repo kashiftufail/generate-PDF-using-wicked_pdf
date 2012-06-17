40709152b1189ea2e5c4d60b5d82cee856bddf61
kashifali17 <kashif.tufail17@gmail.com>
2012-06-17 21:07:34 +0500

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
+  before_filter :get_user
   protect_from_forgery
+
+  def get_user
+    @user = current_user
+  end
+
 end
